09aa7a11752d0bce1d74ee48238e1172236f1905
stundner,gabriel <gabriel.stundner@dell.com>
2024-03-27 23:14:24 -0300

    New Project

---
+++ ConsoleTemplate/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ConsoleTemplate
+{
+    public class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello World!");
+        //}
+
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
